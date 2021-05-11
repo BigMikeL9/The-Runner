a8edd51b06ff619d3880c49509eabbe7aca3043c
MikeL Kamel <mikel1832@yahoo.com>
2021-05-10 20:13:01 -0400

    The-Runner

---
+++ The Runner/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    private float xRange = 25;
+
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Vector3 spawnPos = new Vector3(xRange, 0, 0);
+
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+
+        
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+}
